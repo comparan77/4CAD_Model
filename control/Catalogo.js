928654cfde2a1c7a57ad987208827fd67ac83ff2
Gil <comparan77@gmail.com>
2019-08-15 15:51:56 -0500

    add control for models

---
+++ control/Catalogo.js
@@ -0,0 +1,36 @@
+var Factory = require('../model/Factory.js');
+
+function Catalogo () {};
+
+Catalogo.lstCatalogo = function(strObj, callback) {
+
+    // var strReq = req.originalUrl;
+    // strReq = Common.Capitalize(strReq.replace('/',''));
+    var factory = new Factory();
+    var o = factory.CreateObj(strObj);
+    var oTMng = factory.CreateMng(o);
+
+    oTMng.Action('lst', (data) => {
+      callback(JSON.stringify(data));
+    })
+}
+
+// Transporte tipo
+// Get transporte tipo by linea
+Catalogo.GetTransporteTipoByLinea = function(id, callback) {
+    var factory = new Factory();
+    var o = factory.CreateObj('Transporte_linea_tipo');
+    var oTMng = factory.CreateMng(o);
+
+    oTMng.Select(`select tt.Id, tt.Nombre 
+                    from transporte_linea_tipo tlt 
+                    join transporte_linea tl on
+                    tlt.id_transporte_linea = tl.id
+                    join transporte_tipo tt on
+                    tlt.id_transporte_tipo = tt.id
+                    where tl.id = ?`, id, (data => {
+        callback(JSON.stringify(data));
+    }));
+}
+
+module.exports = Catalogo;
--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-// app.get('/cliente', (req, res) => {
-//     var factory = new Factory();
-//     var o = factory.CreateObj('Cliente');
-//     var oTMng = factory.CreateMng(o);
-    
-//     oTMng.Action('lst', (data) => 
-//     res.send(JSON.stringify(data)))
-// });
-
-// app.get('/aduana', (req, res) => {
-//     var factory = new Factory();
-//     var o = factory.CreateObj('Aduana');
-//     var oTMng = factory.CreateMng(o);
-    
-//     oTMng.Action('lst', (data) => 
-//     res.send(JSON.stringify(data)))
-// });
-
-var Factory = require('./model/Factory.js');
+var Catalogo = require('./control/Catalogo.js')
 var Common = require('../common/Common.js');
 
 const express = require('express')
@@ -29,41 +11,20 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.get('/aduana', (req, res) => Catalogo.lstCatalogo(getStrReq(req), (data) => res.send(data)));
+app.get('/cliente', (req, res) => Catalogo.lstCatalogo(getStrReq(req), (data) => res.send(data)));
+app.get('/documento', (req, res) => Catalogo.lstCatalogo(getStrReq(req), (data) => res.send(data)));
+app.get('/mercancia_vendor', (req, res) => Catalogo.lstCatalogo(getStrReq(req), (data) => res.send(data)));
+app.get('/transporte_linea', (req, res) => Catalogo.lstCatalogo(getStrReq(req), (data) => res.send(data)));
 app.get('/transporte_linea_tipo/:id', (req, res) => {
-
-  var factory = new Factory();
-  var o = factory.CreateObj('Transporte_linea_tipo');
-  var oTMng = factory.CreateMng(o);
-
-  oTMng.Select(`select tt.Id, tt.Nombre 
-                  from transporte_linea_tipo tlt 
-                join transporte_linea tl on
-                  tlt.id_transporte_linea = tl.id
-                join transporte_tipo tt on
-                  tlt.id_transporte_tipo = tt.id
-                where tl.id = ?`, req.params.id, (data => {
-    res.send(JSON.stringify(data));
-  }));
-
+  Catalogo.GetTransporteTipoByLinea(req.params.id, (data) => res.send(data));
 })
 
-app.get('/aduana', (req, res) => lstCatalogo(req, (data) => res.send(data)));
-app.get('/cliente', (req, res) => lstCatalogo(req, (data) => res.send(data)));
-app.get('/documento', (req, res) => lstCatalogo(req, (data) => res.send(data)));
-app.get('/mercancia_vendor', (req, res) => lstCatalogo(req, (data) => res.send(data)));
-app.get('/transporte_linea', (req, res) => lstCatalogo(req, (data) => res.send(data)));
-
-function lstCatalogo(req, callback) {
+function getStrReq(req) {
 
     var strReq = req.originalUrl;
     strReq = Common.Capitalize(strReq.replace('/',''));
-    var factory = new Factory();
-    var o = factory.CreateObj(strReq);
-    var oTMng = factory.CreateMng(o);
-
-    oTMng.Action('lst', (data) => {
-      callback(JSON.stringify(data));
-    })
+    return strReq;
 }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
