738fcedcdf6ec83ca4bb21b957c5bfde3e5dabe3
Gil <comparan77@gmail.com>
2019-08-28 17:10:55 -0500

    ready

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser');
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/aduana', (req, res) => Catalogo.lstCatalogo(getStrReq(req), (data) => res.send(data)));
 app.get('/cliente', (req, res) => Catalogo.lstCatalogo(getStrReq(req), (data) => res.send(data)));
@@ -22,6 +27,11 @@ app.get('/transporte_linea_tipo/:id', (req, res) => {
 app.get('/vendor_mercancia/:id', (req, res) => {
   Catalogo.GetMercanciaByVendor(req.params.id, (data) => res.send(data));
 })
+
+//Post
+app.post('/asn', (req, res) => {
+  res.send(req.body.fecha_arribo);
+});
 
 function getStrReq(req) {
 
